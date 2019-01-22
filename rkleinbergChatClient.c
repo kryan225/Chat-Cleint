db9e99cc6067b937acf26bdadef6c1a23bde4990
Ryan Kleinberg <kryan225.gomets@gmail.com>
2019-01-21 20:38:58 -0800

    can send username

--- rkleinbergChatClient.c
+++ rkleinbergChatClient.c
@@ -3,9 +3,27 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+//https://www.tutorialspoint.com/c_standard_library/c_function_strcat.htm - strcat documentation
+//http://www.cs.rpi.edu/~moorthy/Courses/os98/Pgms/client.c - to look at how to make a sockaddr_in
+//https://www.geeksforgeeks.org/taking-string-input-space-c-3-different-methods/ - scanf documentation
+
+
+
 int main(int argc, const char* argv[])
 {
-	//http://www.cs.rpi.edu/~moorthy/Courses/os98/Pgms/client.c - to look at how to make a sockaddr_in
+
+	char input[100];
+
+
+	char fart[] = "\n";
+	scanf("%[^\n]%*c", &input);
+	strcat(input,fart);
+
+	printf("%s", input);
+	printf("\n");
+
 
 	
 	//define In_addr which will holds the ip address
@@ -32,13 +50,42 @@ int main(int argc, const char* argv[])
 	int connection = connect(mySocket, (struct sockaddr *) &mySockaddr_in, sizeof(mySockaddr_in));
 
 
+
+
+	char data[] = "lawd he comin \n";
+
+	int see = send(mySocket, &input, sizeof(input), 0);
+	int boo = 1;
+	while(boo){
+		
+		scanf("%[^\n]%*c", &input);
+		if(sizeof(input) > 0 ){
+			if(input == "exit"){
+				boo = 0;
+			}else{
+				strcat(input,fart);
+				send(mySocket, input, sizeof(input), 0);
+			}
+			
+		}
+		
+	}
+	
+
+
+
+	printf("\n");
 	printf( "lawd he comin");
 	printf("\n");
-	printf("%d",chat);
+	printf("inet_pton function - should be larger than 0: %d",chat);
 	printf("\n");
-	printf("%d",mySocket);
+	printf("mySocket - should be larger than 0: %d",mySocket);
 	printf("\n");
-	printf("%d",connection);
+	printf("Connection formed - should be >= 0: %d",connection);
+	printf("\n");
+	printf("send function - should not be -1, returns the number of bytes sent: %d",see);
+	printf("\n");
+
 
 	close(mySocket);
 
